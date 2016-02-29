032dd4b69fb7159692ac0a5c7786c562abdcd9e3
Shane <shane8641@live.com>
2016-02-29 03:57:59 -0500

    images change every 5 seconds

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+var index=0;
+function changeBanner(){
+    [].forEach.call(document.images,function (v,i) { document.images[i].hidden = i!==index});
+    index = (index+1) % document.images.length;
+}
+window.onload = function () {setInterval(changeBanner, 5000)};
